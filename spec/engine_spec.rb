70bfaa558c1640fbefc16547efa27e4b0874c315
yorickov <yorickov@gmail.com>
2019-11-11 23:25:18 +0300

    refactor Engine tests

--- spec/engine_spec.rb
+++ spec/engine_spec.rb
@@ -9,6 +9,7 @@ describe Engine do
     deck1 = []
     52.times { deck1 << ['6+', 6] }
     @cards.cards = deck1
+
     @diller1 = Diller.new('Sam')
     @diller2 = Diller.new('Sarah')
     @engine = Engine.new(@cards, @diller1, @diller2)
@@ -16,25 +17,20 @@ describe Engine do
 
   it 'creation' do
     expect(@engine.status).to eq(-1)
-    expect(@engine.players[0].hand_sum).to eq(12)
-    expect(@engine.players[1].hand_sum).to eq(12)
+    expect(@engine.players.all? { |p| p.hand_sum == 12 }).to be_truthy
   end
 
   it 'process' do
     @engine.process
     @engine.process
     expect(@engine.status).to eq(2)
-    expect(@engine.players[0].hand_sum).to eq(18)
-    expect(@engine.players[1].hand_sum).to eq(18)
-    expect(@engine.players[0].choice).to eq('added')
-    expect(@engine.players[1].choice).to eq('added')
-    expect(@engine.players[0].deposit).to eq(100)
-    expect(@engine.players[1].deposit).to eq(100)
+    expect(@engine.players.all? { |p| p.hand_sum == 18 }).to be_truthy
+    expect(@engine.players.all? { |p| p.choice == 'added' }).to be_truthy
+    expect(@engine.players.all? { |p| p.deposit == 100 }).to be_truthy
 
     @engine.init
     expect(@engine.status).to eq(-1)
-    expect(@engine.players[0].choice).to eq('skip')
-    expect(@engine.players[0].deposit).to eq(90)
-    expect(@engine.players[1].deposit).to eq(90)
+    expect(@engine.players.all? { |p| p.choice == 'skip' }).to be_truthy
+    expect(@engine.players.all? { |p| p.deposit == 90 }).to be_truthy
   end
 end
