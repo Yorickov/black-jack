d1a31e6424d62b93ff9f1be9d1d47c14d9b5b55e
yorickov <yorickov@gmail.com>
2019-11-12 19:40:05 +0300

    add validation test

--- spec/player_spec.rb
+++ spec/player_spec.rb
@@ -7,4 +7,12 @@ describe Player do
     player = Player.new('Sam').init
     expect(player.choice).to eq('skip')
   end
+
+  it 'presence' do
+    player = Player.new('John')
+    expect(player.valid?).to be_truthy
+
+    expect { Player.new('   ') }
+      .to raise_error(ArgumentError, 'You must input smth.')
+  end
 end
